7757f517371aad2bd11ea98e6371edac291e8f24
n-ambati <42886667+n-ambati@users.noreply.github.com>
2019-06-11 14:53:36 +0530

    popup for required fields

--- scripts.js
+++ scripts.js
@@ -40,7 +40,6 @@ var _CSV_KEYVAL_SEP = ':';
 var _IMPORT_CSV_COMMENT_CHAR = '#';
 
 var _img_metadata = {};   // data structure to store loaded images metadata
-var _img_region_id = 1;
 var _img_count    = 0;    // count of the loaded images
 var _canvas_regions = []; // image regions spec. in canvas space
 var _canvas_scale   = 1.0;// current scale of canvas image
@@ -172,9 +171,6 @@ function ImageRegion() {
   this.is_user_selected  = false;
   this.shape_attributes  = {}; // region shape attributes
   this.region_attributes = {}; // region attributes
-  this.image_region_id = _img_region_id;
-  console.log("image region id: " + _img_region_id);
-  _img_region_id++;
 }
 
 function populate_region_list(legendList, label_id) {
@@ -305,7 +301,6 @@ function set_variables(){
  _IMPORT_CSV_COMMENT_CHAR = '#';
 
  _img_metadata = {};   // data structure to store loaded images metadata
- _img_region_id = 1;   // image id
  _img_count    = 0;    // count of the loaded images
  _canvas_regions = []; // image regions spec. in canvas space
  _canvas_scale   = 1.0;// current scale of canvas image
@@ -430,9 +425,10 @@ function populate_attribute_position(attribute_id){
 
   // <div onclick="toggle_attributes_input_panel()" class="attributes_panel_button">&times;</div>
   var body = document.getElementById(attribute_id);
-  var boxContainer = document.createElement("div");
-  boxContainer.setAttribute("onclick", "toggle_attributes_input_panel()");
-  boxContainer.setAttribute("style", "width: 10px;color: black;font-size: x-large;margin-left: 0.5em;padding: 0;");
+  var boxContainer = document.createElement("button");
+  boxContainer.setAttribute("type", "btn btn-primary");
+  boxContainer.setAttribute("onclick", "populate_popup()");
+  boxContainer.setAttribute("style", "color: white;font-size: 20px;margin-left: 0.5em;padding: 5px; background-color: black;");
   boxContainer.innerHTML = "&times;";
   var center = document.createElement("center");
   var warn = document.createElement("span");
@@ -441,9 +437,38 @@ function populate_attribute_position(attribute_id){
   body.appendChild(boxContainer);
   center.appendChild(warn);
   body.appendChild(center);
-  
+
 }
 
+function populate_popup() {
+  //Check for condition
+  let container = document.getElementById("canvas_panel");
+  let _modal_ = document.createElement("DIV");
+  _modal_.setAttribute("id", "popup");
+  _modal_.style = "display: block; position: fixed; z-index: 5; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.8);"
+  let _modal_content = document.createElement("DIV");
+  _modal_content.setAttribute("id", "popup-content");
+  _modal_content.style = "background-color: rgb(245, 245, 245); margin: 10% auto; padding: 40px; border: 1px solid #888; width: 40%; height: 20%; color:red;";
+  _modal_content.innerHTML = "<p>Please enter the required (<b>*</b>) fields.</p>"
+  let _modal_btn = document.createElement("button");
+  _modal_btn.setAttribute("type", "btn btn-primary");
+  _modal_btn.innerHTML = "close";
+  _modal_btn.style.float = "right";
+  _modal_btn.style = "font-size: 18px; margin-top: 60px; background-color: black; color: white; float:right;";
+  let _modal_btn_2 = document.createElement("button");
+  _modal_btn_2.setAttribute("type", "btn btn-primary");
+  _modal_btn_2.innerHTML = "clear";
+  _modal_btn_2.style.float = "right";
+  _modal_btn_2.style = "font-size: 18px; margin-top: 60px; background-color: black; color: white; margin-left: 380px;";
+  _modal_content.appendChild(_modal_btn);
+  _modal_content.appendChild(_modal_btn_2);
+  _modal_.appendChild(_modal_content);
+  container.appendChild(_modal_);
+
+  _modal_btn.onclick = function(){
+    _modal_.style.display = "none";
+  }
+}
 
 function _init(event) {
   init_canvas(event["canvas_id"], event["region_canvas_id"]);
@@ -3689,7 +3714,7 @@ function init_spreadsheet_input(type, col_headers, data, attr_id, row_names) {
     } else {
       region_traversal_order = all_reg_list;
     }
-  
+
 
 
   var attrtable = document.createElement('table');
@@ -3701,11 +3726,11 @@ function init_spreadsheet_input(type, col_headers, data, attr_id, row_names) {
   topleft_cell.innerHTML = '';
   topleft_cell.style.border = 'none';
 
-  // console.log("data---");
-  // console.log(data);
-  // console.log(type);
-  // console.log(sel_reg_list);
-  // console.log("----");
+  console.log("data---");
+  console.log(data);
+  console.log(type);
+  console.log(sel_reg_list);
+  console.log("----");
   for (var col_header in _region_attributes ) {
     if(col_header == data[sel_reg_list[0]].shape_attributes.type){
       for (var col in _region_attributes [col_header]){
@@ -3737,8 +3762,7 @@ function init_spreadsheet_input(type, col_headers, data, attr_id, row_names) {
       if ( sel_reg_list.length ) {
         row_i = region_traversal_order[row_i];
       }
-      di = {}
-      di[data[row_i].shape_attributes.type] = "";
+      di = data[row_i].region_attributes;
     } else {
       di = data[row_i];
     }
@@ -3762,34 +3786,35 @@ function init_spreadsheet_input(type, col_headers, data, attr_id, row_names) {
     // console.log("-------");
 
     for ( var key in col_headers ) {
-      var input_id = type[0] + '#' + key + '#' + _img_metadata[_image_id].regions[row_i].image_region_id;
+      var input_id = type[0] + '#' + key + '#' + row_i;
           if ( di.hasOwnProperty(key) ){
-          // var ip_val = di[key];
+          var ip_val = di[key];
           for (var col in _region_attributes [key]){
             if(attributes_values.hasOwnProperty(input_id+'#'+col)){
                 row.insertCell(-1).innerHTML = '<input type="text"' +
                 ' id="' + input_id + '#'+ col + '"' +
                 ' value="' + attributes_values[input_id+'#'+col] + '"' +
-                ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', \'' + _region_attributes[key][col].att_name + '\', \'' + row_i + '\', this.value)" ' +
+                ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', this.value)" ' +
                 ' onblur="attr_input_blur(' + row_i + ')"' +
                 ' onfocus="attr_input_focus(' + row_i + ');" />';
             }
             else{
                   row.insertCell(-1).innerHTML = '<input type="text"' +
                   ' id="' + input_id + '#'+ col + '"' +
-                  ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', \'' + _region_attributes[key][col].att_name + '\', \'' + row_i + '\', this.value)" ' +
+                  ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', this.value)" ' +
                   ' onblur="attr_input_blur(' + row_i + ')"' +
                   ' onfocus="attr_input_focus(' + row_i + ');" />';
             }
           }
-          
+
         }
       else{
       if ( key == data[sel_reg_list[0]].shape_attributes.type ) {
         for (var col in _region_attributes [key]){
           row.insertCell(-1).innerHTML = '<input type="text"' +
           ' id="' + input_id + '#'+ col + '"' +
-          ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', \'' + _region_attributes[key][col].att_name + '\', \'' + row_i + '\', this.value)" ' +          ' onblur="attr_input_blur(' + row_i + ')"' +
+          ' onchange="update_attribute_value(\'' + input_id + '#' + col + '\', this.value)" ' +
+          ' onblur="attr_input_blur(' + row_i + ')"' +
           ' onfocus="attr_input_focus(' + row_i + ');" />';
         }
       }
@@ -3994,19 +4019,8 @@ function update_file_attributes_input_panel() {
                          [_current_image_filename]);
 }
 
-
-function required_attributes_filled(){
-  var seg_rel_list = [];
-  for (var i in _img_metadata[_image_id].regions){
-    if ( _img_metadata[_image_id].regions[i].is_user_selected ){
-      seg_rel_list.push(i);
-    }
-  }
-
-}
-
 function toggle_attributes_input_panel() {
-  if( _is_reg_attr_panel_visible ) { // && required_attributes_filled()
+  if( _is_reg_attr_panel_visible ) {
     toggle_reg_attr_panel();
   }
   if( _is_file_attr_panel_visible ) {
@@ -4038,7 +4052,7 @@ function toggle_reg_attr_panel() {
         attributes_panel.focus();
       }
     } else {
-     
+
       update_region_attributes_input_panel();
        _is_attributes_panel_visible = true;
       _is_reg_attr_panel_visible = true;
@@ -4086,11 +4100,11 @@ function update_vertical_space() {
   panel.style.height = attributes_panel.offsetHeight+'px';
 }
 
-function update_attribute_value(attr_id, att_name, regionId, value) {
+function update_attribute_value(attr_id, value) {
   var attr_id_split = attr_id.split('#');
   var type = attr_id_split[0];
-  var attribute_name = att_name;
-  var region_id = regionId;
+  var attribute_name = attr_id_split[1];
+  var region_id = attr_id_split[2];
 
   switch(type) {
   case 'r': // region attribute
